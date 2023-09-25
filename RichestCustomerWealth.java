5ca15bed0a1ff377932553cc8e3636d556f4c373
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2023-09-25 16:04:31 -0400

    Add files via upload

---
+++ RichestCustomerWealth.java
@@ -0,0 +1,12 @@
+class Solution {
+	public int maximumWealth(int[][] accounts) {
+		int returnVal = 0;
+
+		int rows = accounts.length;
+		int cols = accounts[0].length;
+
+		for (int i = 0; i < rows; i++) {
+			
+		}
+	}
+}
