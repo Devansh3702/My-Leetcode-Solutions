14aa9f57c69c91468db68da0a131df0087fe14fc
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2023-09-25 16:05:38 -0400

    Update RichestCustomerWealth.java

--- RichestCustomerWealth.java
+++ RichestCustomerWealth.java
@@ -6,7 +6,14 @@ class Solution {
 		int cols = accounts[0].length;
 
 		for (int i = 0; i < rows; i++) {
-			
+			int currentCustomerVal = 0;
+			for (int j = 0; j < cols; j++) {
+				currentCustomerVal += accounts[i][j];
+			}
+			if (returnVal < currentCustomerVal) {
+				returnVal = currentCustomerVal;
+			}
 		}
+		return returnVal;
 	}
 }
