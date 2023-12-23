c2687445bb2fb56a05b6eea1adde913af2c75480
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2023-12-22 20:57:16 -0500

    Add files via upload

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,16 @@
+// Bottom-Up DP - Optimized
+// Time: O(n)
+// Space: O(1)
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return maxProfit;
+    }
+}
