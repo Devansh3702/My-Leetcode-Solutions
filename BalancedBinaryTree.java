049dbcf688cfcbc9fa3558528cdb9bec194f1a03
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2023-12-26 23:26:15 -0500

    Add files via upload

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,22 @@
+import javax.swing.tree.TreeNode;
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return -1 != dfs(root);
+    }
+
+    public static int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(left, right);
+    }
+}
+
+
