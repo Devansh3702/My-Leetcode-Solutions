9455c2dede84036e41eba2a7998adff128cd35d8
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-01-03 20:26:33 -0500

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 
 // Approach: Majority element occurs more than n/2 times that's why it is confirmed that it will be on mid in sorted array
+// Time: O(n log n) due to Arrays.sort().
+// Space: O(n) WCRT, O(log n) ACRT (both due to Arrays.sort() method).
 class Solution {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
