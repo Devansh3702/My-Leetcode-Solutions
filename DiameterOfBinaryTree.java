fb2d241108b66c617174751586c92365cae5d390
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-01-05 22:55:13 -0500

    Add files via upload

---
+++ DiameterOfBinaryTree.java
@@ -0,0 +1,23 @@
+import javax.swing.tree.TreeNode;
+
+// Optimal Solution
+// Time: O(n)
+// Space: O(1)
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        int[] array = {0};
+        int tmp = getDiameter(root, array, true);
+        return array[0];
+    }
+
+    public int getDiameter(TreeNode root, int[] array, boolean val) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = getDiameter(root.left, array, false);
+        int rightDepth = getDiameter(root.right, array, false);
+        array[0] = Math.max(array[0], leftDepth + rightDepth);
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
