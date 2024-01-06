be1040816c78d5b7035d6f801518e2790a13eb29
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-01-05 22:56:34 -0500

    Update DiameterOfBinaryTree.java

--- DiameterOfBinaryTree.java
+++ DiameterOfBinaryTree.java
@@ -4,17 +4,17 @@
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
         int[] array = {0};
-        int tmp = getDiameter(root, array, true);
+        int tmp = getDiameter(root, array);
         return array[0];
     }
 
-    public int getDiameter(TreeNode root, int[] array, boolean val) {
+    public int getDiameter(TreeNode root, int[] array) {
         if (root == null) {
             return 0;
         }
 
-        int leftDepth = getDiameter(root.left, array, false);
-        int rightDepth = getDiameter(root.right, array, false);
+        int leftDepth = getDiameter(root.left, array);
+        int rightDepth = getDiameter(root.right, array);
         array[0] = Math.max(array[0], leftDepth + rightDepth);
         return 1 + Math.max(leftDepth, rightDepth);
     }
