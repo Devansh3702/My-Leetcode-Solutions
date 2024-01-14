b2fa130cd94593ba6578be131c9359073c120690
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-01-14 14:29:03 -0500

    Add files via upload

---
+++ ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,22 @@
+import javax.swing.tree.TreeNode;
+import java.util.Arrays;
+
+// Time: O(n)
+// Space: O(log n)
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return helper(nums, 0, nums.length - 1);
+    }
+
+    public static TreeNode helper(int[] nums, int low, int high) {
+        if (high < low) {
+            return null;
+        }
+
+        int mid = (low + high) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = helper(nums, low, mid - 1);
+        root.right = helper(nums, mid + 1, high);
+        return root;
+    }
+}
