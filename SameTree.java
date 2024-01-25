02001d729a54257b8fdc785c864ce6a78c8b131a
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-01-24 22:48:37 -0500

    Add files via upload

---
+++ SameTree.java
@@ -0,0 +1,16 @@
+import javax.swing.tree.TreeNode;
+
+// Time: O(n)
+// Space: O(1) ignoring the recursion call stack
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
