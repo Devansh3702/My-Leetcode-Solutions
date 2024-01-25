c302b71a3576099cc6661d5df4a1f302b3147364
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-01-25 17:54:18 -0500

    Add files via upload

---
+++ SymmetricTree.java
@@ -0,0 +1,20 @@
+import javax.swing.tree.TreeNode;
+
+// Time: O(n)
+// Space: O(1) if ignoring recursion call stack, else its O(h) [between O(log n) and O(n)]
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return helper(root.left, root.right);
+    }
+
+    public boolean helper(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+
+        return p.val == q.val && helper(p.left, q.right) && helper(p.right, q.left);
+    }
+}
