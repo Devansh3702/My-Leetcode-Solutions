8cd547af76ac505a4dd3a10e2616e51831b2b074
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-01-27 11:09:55 -0500

    Add files via upload

---
+++ PascalsTriangle2.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// Time:  O(n^2) WCRT
+// Space: O(n^2)
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        return generate(rowIndex + 1).get(rowIndex);
+    }
+
+    public static List<List<Integer>> generate(int numRows) {
+
+        int n = 0;
+        List<List<Integer>> retVal = new ArrayList<>();
+        while (n < numRows) {
+            List<Integer> list = new ArrayList<>();
+            if (n == 0) {
+                list.add(1);
+            } else {
+                List<Integer> prevList = retVal.get(retVal.size() - 1);
+                int targetListSize = n + 1;
+                list.add(1);
+                for (int i = 0; i < targetListSize - 2; i++) {
+                    list.add(prevList.get(i) + prevList.get(i + 1));
+                }
+                list.add(1);
+            }
+            retVal.add(list);
+            n++;
+        }
+
+        return retVal;
+    }
+}
