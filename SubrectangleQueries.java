e8c79c4bbcb18646f5c2da1fe65bdc4fae34fc54
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-01-29 21:32:46 -0500

    Update SubrectangleQueries.java

--- SubrectangleQueries.java
+++ SubrectangleQueries.java
@@ -1,12 +1,14 @@
 class SubrectangleQueries {
 
-    private int[][] subRectangle;       // Space: O(row x col)
+    private int[][] subRectangle;    // Space: O(row x col)
 
-    public SubrectangleQueries(int[][] rectangle) {     // O(1))
+    public SubrectangleQueries(int[][] rectangle) {     
+        // Running Time: O(1))
         subRectangle = rectangle;
     }
 
-    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {      // O(row x col) WCRT
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {      
+        // Running Time: O(row x col) WCRT
         for (int i = row1; i <= row2; i++) {
             for (int j = col1; j <= col2; j++) {
                 subRectangle[i][j] = newValue;
@@ -14,7 +16,8 @@ class SubrectangleQueries {
         }
     }
 
-    public int getValue(int row, int col) {             // O(1)
+    public int getValue(int row, int col) {             
+        // Running Time: O(1)
         return subRectangle[row][col];
     }
 }
