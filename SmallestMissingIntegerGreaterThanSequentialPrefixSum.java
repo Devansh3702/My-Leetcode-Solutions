9f4286be6549f0c6a857886453eda28f1fb6fc9d
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-02-10 11:18:08 -0500

    Add files via upload

---
+++ SmallestMissingIntegerGreaterThanSequentialPrefixSum.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+// Time: O(n log n)
+// Space: O(1)
+class Solution {
+    public int missingInteger(int[] nums) {
+        int n = nums.length;
+        int pre = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                pre += nums[i];
+            }
+            else {
+                break;
+            }
+        }
+        Arrays.sort(nums);
+        for (int num : nums) {
+            if (num == pre) {
+                pre++;
+            }
+        }
+        return pre;
+    }
+}
