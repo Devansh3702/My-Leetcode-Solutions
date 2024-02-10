b3979159fd5728d2e78f51c60899cece4dcda1ff
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-02-10 11:31:12 -0500

    Add files via upload

---
+++ TypeOfTriangle2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        }
+
+        boolean isValidTriangle = nums[0] + nums[1] > nums[2] && nums[1] + nums[2] > nums[0] && nums[2] + nums[0] > nums[1];
+
+        if (isValidTriangle) {
+            if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) {
+                return "isosceles";
+            }
+            return "scalene";
+        }
+        return "none";
+    }
+}
