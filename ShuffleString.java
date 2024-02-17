f80446de540613ff37f8e5bfe01efda76dbec84d
Devansh Shah <98128611+Devansh3702@users.noreply.github.com>
2024-02-17 13:15:41 -0500

    Add files via upload

---
+++ ShuffleString.java
@@ -0,0 +1,13 @@
+// Time: O(n)
+// Space: O(n)
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] arr = new char[s.length()];
+
+        for (int i = 0; i < indices.length; i++) {
+            arr[indices[i]] = s.charAt(i);
+        }
+
+        return String.valueOf(arr);
+    }
+}
